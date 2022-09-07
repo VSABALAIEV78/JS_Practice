3c9d5efb091cbf57be73eee791f9d27f50e7dade
VadymS <sabalayev1@mail.com>
2022-09-07 11:25:47 +0300

    My first commit

---
+++ VS1/script.js
@@ -0,0 +1,36 @@
+// alert("q23");
+// console.log(123);
+
+// const answer = prompt('Do you like JS?', 'YEAP');
+// console.log(answer);
+// console.log(typeof(answer));
+
+
+// const mass1 = [];
+
+// mass1[0] = prompt('Enter your First Name', '');
+// mass1[1] = prompt('Enter your Last Name', '');
+// mass1[2] = +prompt('Enter your Age', '');
+
+// console.log(mass1);
+// console.log(typeof(mass1));
+
+
+const name1 = 'Vadick';
+const name2 = 'Vika';
+const someObject = 'ct/1022/'; // Monitors
+
+const MonitorPart1 = 'md'; // Monitor1
+const MonitorPart2 = '4113997'; // Monitor2
+
+//alert('Privet, '+ name1); //запишет строку + имя
+
+// Интерполяция ES6
+//alert(`Privet, ${name2}`) // запишет строку + имя - проще поддерживать
+
+console.log(`https://pn.com.ua/${someObject}`);
+
+console.log(`https://pn.com.ua/${MonitorPart1}/${MonitorPart2}`);
+
+
+console.log(2*2 !== +'4');
