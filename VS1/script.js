84a292036e589df4c3e4131a565e5fa76dc6e910
Vadim <sabalayev1@gmail.com>
2022-12-05 20:11:01 +0200

    cases with IF operator

--- VS1/script.js
+++ VS1/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -15,3 +17,42 @@ personalMovieDB.movies[filmName] = score;
 
 // got a solution. works with [], but not with the dot > movies.filmName v.2
 console.log(personalMovieDB);
+
+
+
+//operators IF 
+
+const number = 19;
+
+if (number > 20) {
+    console.log('in bigger than 20');
+} else if (number < 10) {
+    console.log('in smaller that 10');
+} else if (number >= 10 && number < 19) {
+    console.log('number in range of 10 - 19');
+} else {
+    console.log('result = ' + number);
+}
+
+
+const num1 = 10;
+(num1 > 8) && (num1 < "15") ? console.log(`correct = ${num1}`): console.log('error');
+
+const num2 = 180; //строгое сравнение всегда
+switch (num2) {
+    case 9:
+        console.log('incorrect');
+        break;
+    case 11:
+        console.log('incorrect');
+        break;
+    case '10':
+        console.log('incorrect types');
+        break;
+    case 10:
+        console.log('correct');
+        break;
+    default:
+        console.log('wtf?');
+        break;
+}
