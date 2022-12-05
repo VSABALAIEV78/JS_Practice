4f85824a01cc9655e02b636a9eed82f823a52c10
Vadim <sabalayev1@gmail.com>
2022-12-05 14:11:03 +0200

    Task 19. Solved. Final ver.

--- VS1/script.js
+++ VS1/script.js
@@ -1,18 +1,12 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-    count: '',
-    movies: {
-
-    },
-    actors: {
-
-    },
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
     genres: [],
     privat: false
 };
-
-personalMovieDB.count = numberOfFilms;
 
 const filmName = prompt('Один из последних посмотренных фильмов?', '');
 const score = +prompt('На сколько оцените его?', '');
@@ -20,4 +14,4 @@ const score = +prompt('На сколько оцените его?', '');
 personalMovieDB.movies[filmName] = score;
 
 // got a solution. works with [], but not with the dot > movies.filmName v.2
-console.log(personalMovieDB.movies);
+console.log(personalMovieDB);
