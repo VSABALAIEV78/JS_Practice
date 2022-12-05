92ae88336a84c50ae806da14efff5b85018c4f35
Vadim <sabalayev1@gmail.com>
2022-12-05 11:42:57 +0200

    1ts step of task # 19

--- VS1/script.js
+++ VS1/script.js
@@ -1,74 +1,11 @@
-// alert("q23");
-// console.log(123);
+const numberOfFilms = {
+    count: '',
+    movies: {
 
-// const answer = prompt('Do you like JS?', 'YEAP');
-// console.log(answer);
-// console.log(typeof(answer));
+    },
+    actors: {
 
-
-// const mass1 = [];
-
-// mass1[0] = prompt('Enter your First Name', '');
-// mass1[1] = prompt('Enter your Last Name', '');
-// mass1[2] = +prompt('Enter your Age', '');
-
-// console.log(mass1);
-// console.log(typeof(mass1));
-
-
-const name1 = 'Vadick';
-const name2 = 'Vika';
-const someObject = 'ct/1022/'; // Monitors
-
-const MonitorPart1 = 'md'; // Monitor1
-const MonitorPart2 = '4113997'; // Monitor2
-
-//alert('Privet, '+ name1); //запишет строку + имя
-
-// Интерполяция ES6
-//alert(`Privet, ${name2}`) // запишет строку + имя - проще поддерживать
-
-console.log(`https://pn.com.ua/${someObject}`);
-
-console.log(`https://pn.com.ua/${MonitorPart1}/${MonitorPart2}`);
-
-
-console.log(2 * 2 !== +'4');
-
-
-const webSite = 'md/2011825';
-const myCode = 'is_crazy';
-
-//console.log('https://pn.com.ua/' + webSite);
-
-console.log(`https://pn.com.ua/${webSite}/${myCode}}`);
-
-
-const number1 = 5;
-const number2 = '11';
-let result01;
-
-console.log(result01 = number1 + number2 + 0.11);
-
-let yourAge = +'55';
-
-console.log(yourAge);
-console.log(typeof (yourAge));
-
-
-let incr = 10,
-    decr = 10;
-
-incr++;
-decr--;
-console.log(incr, decr);
-
-
-let a1 = 3,
-    a2 = 2,
-    c;
-
-console.log(c = a1 % a2);
-console.log(9 % 4);
-
-let gitVS;
+    },
+    genres: [],
+    privat: false
+};
