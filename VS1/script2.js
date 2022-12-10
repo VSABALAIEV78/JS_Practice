7aefed49a89dea1c630d49953d12bf16d6286053
Vadim <sabalayev1@gmail.com>
2022-12-10 17:39:52 +0200

    created file for function prsctice

--- VS1/script2.js
+++ VS1/script2.js
@@ -4,40 +4,6 @@ let numberOfFilms,
     filmName,
     score;
 
-// 1st question
-first: for (let i = 0; i < i + 1; i++) {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели? Введите число от 1', '');
-    if (numberOfFilms > 0) {
-        break first;
-    }
-}
-console.log(`Вопрос №1. Фильмов: ${numberOfFilms}`);
-
-// 2nd question
-second: for (let i = 0; i < i + 1; i++) {
-    filmName = prompt('Вопрос 2. Один из последних посмотренных фильмов?\nОт 4х символов', '');
-
-    if (filmName === null) {
-        filmName = prompt('Вопрос 2. Один из последних посмотренных фильмов?\nОт 4х символов', '');
-    } else if (filmName.length > 3 && filmName.length < 50) {
-        break second;
-    }
-}
-console.log(`Вопрос №2. Наименование фильма: ${filmName}`);
-
-// 3rd question
-third: for (let i = 0; i < i + 1; i++) {
-    score = prompt('На сколько оцените его? (от 1 до 10)', '');
-
-    if (score > 0 && score <= 10) {
-        break third;
-    }
-}
-// solved steps 1-3 - commited
-
-console.log(`Вопрос №3. Рейтинг: ${score}`);
-
-
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -45,6 +11,45 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+
+// 1st question
+first: for (let i = 0; i < 1; i++) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели? Введите число от 1', '');
+    if (numberOfFilms > 0) {
+        personalMovieDB.count = numberOfFilms;
+        break first;
+    } else {
+        i--;
+    }
+}
+console.log(`Вопрос №1. Фильмов: ${numberOfFilms}`);
+
+// 2nd question
+second: for (let i = 0; i < 1; i++) {
+    filmName = prompt('Вопрос 2. Один из последних посмотренных фильмов?\nОт 4х символов', '');
+
+    if (filmName != null && filmName.length > 3 && filmName.length < 50) {
+        //break second;
+    } else {
+        i--;
+    }
+}
+console.log(`Вопрос №2. Наименование фильма: ${filmName}`);
+
+// 3rd question
+third: for (let i = 0; i < 1; i++) {
+    score = prompt('На сколько оцените его? (от 1 до 10)', '');
+
+    if (score > 0 && score <= 10) {
+        personalMovieDB.movies[filmName] = score;
+        break third;
+    } else {
+        i--;
+    }
+}
+// solved steps 1-3 - commited
+
+console.log(`Вопрос №3. Рейтинг: ${score}`);
 
 for (let i = 0; i < 1; i++) {
     if (personalMovieDB.count < 10) {
@@ -57,4 +62,6 @@ for (let i = 0; i < 1; i++) {
         console.log('error vs');
     }
 }
-// finished task # 24.2
+
+console.log(personalMovieDB);
+// finished task # 24.2 updated
