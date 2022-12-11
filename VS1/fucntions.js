a5a9864c5f70eeb234bf2996777f43ea62a01450
Vadim <sabalayev1@gmail.com>
2022-12-11 21:05:40 +0200

    Task # 29. Functions train coupe. Fixed trivial bugs in my code

--- VS1/fucntions.js
+++ VS1/fucntions.js
@@ -7,22 +7,24 @@ function calculateVolumeAndArea(side) {
         sCube;
 
     for (let i = 0; i < 1; i++) {
+        let result;
 
-        if (typeof (side) == 'string' || side <= 0 || side == '' || side == null ||
+        if (typeof (side) === 'string' || side <= 0 || side === '' || side === null ||
             !Number.isInteger(side)) {
 
-            console.log(`При вычислении произошла ошибка`);
+            result = `При вычислении произошла ошибка`;
 
         } else {
             volume = side * side * side;
             sCube = (side * side) * 6;
-            console.log(`Объем куба: ${volume}, площадь всей поверхности = ${sCube}`);
+            result = `Объем куба: ${volume}, площадь всей поверхности: ${sCube}`;
+
         }
-        return volume, sCube;
+        return result;
     }
 
 }
-calculateVolumeAndArea(4);
+//console.log(calculateVolumeAndArea(5.2));
 
 //task 29.2
 
@@ -31,16 +33,16 @@ function getCoupeNumber(seatNum) {
         let coupe;
 
         if (typeof (seatNum) === 'string' || seatNum < 0 || !Number.isInteger(seatNum)) {
-            console.log('Ошибка. Проверьте правильность введенного номера места');
-        } else if (seatNum == 0 || seatNum > 37) {
-            console.log('Таких мест в вагоне не существует');
+            coupe = 'Ошибка. Проверьте правильность введенного номера места';
+        } else if (seatNum === 0 || seatNum > 36) {
+            coupe = 'Таких мест в вагоне не существует';
         } else {
             coupe = parseInt((seatNum - 1) / 4 + 1);
-            console.log(coupe);
         }
+        return coupe;
     }
 }
 
-getCoupeNumber(33);
+console.log(getCoupeNumber(37));
 
 // solved with small hint with formula >>> (seatNum -1) / 4 + 1
