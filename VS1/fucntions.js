3a7624ae96c61a3416feb8034f63f2a9e1c0e1f6
Vadim <sabalayev1@gmail.com>
2022-12-11 20:44:33 +0200

    Task # 29. Functions -  detect Train coupe by seatNumber > solved

--- VS1/fucntions.js
+++ VS1/fucntions.js
@@ -2,7 +2,7 @@
 
 //task 29.1
 
-function findVolumeSCube(side) {
+function calculateVolumeAndArea(side) {
     let volume,
         sCube;
 
@@ -22,7 +22,25 @@ function findVolumeSCube(side) {
     }
 
 }
-findVolumeSCube(4);
-
+calculateVolumeAndArea(4);
 
 //task 29.2
+
+function getCoupeNumber(seatNum) {
+    for (let i = 0; i < 1; i++) {
+        let coupe;
+
+        if (typeof (seatNum) === 'string' || seatNum < 0 || !Number.isInteger(seatNum)) {
+            console.log('Ошибка. Проверьте правильность введенного номера места');
+        } else if (seatNum == 0 || seatNum > 37) {
+            console.log('Таких мест в вагоне не существует');
+        } else {
+            coupe = parseInt((seatNum - 1) / 4 + 1);
+            console.log(coupe);
+        }
+    }
+}
+
+getCoupeNumber(33);
+
+// solved with small hint with formula >>> (seatNum -1) / 4 + 1
