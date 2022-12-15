a3ae9ae837dae053331b6f8777c223240b0b7449
Vadim <sabalayev1@gmail.com>
2022-12-15 11:20:07 +0200

    Task # 35.10 decomposition of {} + functions > solved

--- VS1/practiceFile.js
+++ VS1/practiceFile.js
@@ -1,105 +1,51 @@
 'use strict';
 
-let numberOfFilms,
-    filmName,
-    score;
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+const personalPlanPeter = {
+    name: "Peter",
+    age: "249",
+    skills: {
+        languages: ['ru', 'eng', 'ua'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function (plan) {
+        let lang = plan.skills.languages.join(' ');
+        lang = lang.toUpperCase();
+        return `Мне ${plan.age} и я владею языками: ${lang}`;
+    }
 };
 
-function start() {
-    for (let i = 0; i < 1; i++) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели? Введите число от 1', '');
-        if (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-            i--;
-        }
-    }
-    personalMovieDB.count = numberOfFilms;
+
+// taks 1
+function showExperience(plan) {
+    const {
+        exp
+    } = plan.skills;
+    return exp;
 }
 
-start();
+console.log(showExperience(personalPlanPeter));
 
-function rememberMyFilms() {
-    // cycle 2
-    for (let j = 0; j < 1; j++) {
-        filmName = prompt('Один из последних посмотренных фильмов?', '');
-        if (filmName == null || filmName == '' || filmName.length < 4 || filmName.length >= 50) {
-            j--;
-        }
+// task 2
 
-    }
-    // cycle 3
-    for (let i = 0; i < 1; i++) {
-        score = +prompt('На сколько оцените его? (от 1 до 10)', '');
-
-        if (score == null || score == '' || isNaN(score) || score <= 0 || score > 10) {
-            i--;
-        }
-    }
-
-    personalMovieDB.movies[filmName] = score;
-
-    console.log(`Вопрос №1. Фильмов: ${numberOfFilms}`);
-    console.log(`Вопрос №2. Наименование фильма: ${filmName}`);
-    console.log(`Вопрос №3. Рейтинг: ${score}`);
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-
-    for (let i = 0; i < 1; i++) {
-        if (personalMovieDB.count < 10) {
-            console.log(`Результат: Просмотрено довольно мало фильмов: ${personalMovieDB.count}`);
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-            console.log(`Результат: Вы классический зритель: ${personalMovieDB.count} фильмов`);
-        } else if (personalMovieDB.count >= 30) {
-            console.log(`Результат: Вы киноман: ${personalMovieDB.count} фильмов`);
+function showProgrammingLangs(plan) {
+    let programmingLanguages = '';
+    for (let key in plan.skills.programmingLangs) {
+        if (typeof ([key]) !== undefined) {
+            programmingLanguages += `Язык ${key} изучен на ${plan.skills.programmingLangs[key]}\n`;
         } else {
-            console.log('Результат: error vs');
+            programmingLanguages = '4';
         }
     }
-
+    return programmingLanguages;
 }
 
-detectPersonalLevel();
+console.log(showProgrammingLangs(personalPlanPeter));
 
+//task 3
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
 
-function showMyDB() {
-    if (personalMovieDB.privat === false) {
-        console.log(personalMovieDB);
-    }
-}
-
-//showMyDB();
-
-function showMyDB1(state) {
-    if (!state) {
-        console.log(personalMovieDB);
-        console.log('privat has state = false');
-    }
-}
-
-showMyDB1();
-
-
-function writeMyGenres() {
-
-    for (let i = 0; i < 3; i++) {
-        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр № ${i+1}`, '');
-
-        if (personalMovieDB.genres[i] == null || personalMovieDB.genres[i] == '') {
-            i--;
-        }
-    }
-
-}
-
-writeMyGenres();
-
-console.log(personalMovieDB);
+//solved Task # 35.10 decomposition of {} + functions 
