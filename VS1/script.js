f693b348e8268aaa55234c422f198d0f802ce8c4
Vadim <sabalayev1@gmail.com>
2023-01-03 11:28:34 +0200

    2023 started

--- VS1/script.js
+++ VS1/script.js
@@ -1,85 +1,147 @@
 'use strict';
 
-//Task # 37. 13
+// //Task # 37. 13
 
-const shoppingMallData = {
-    shops: [{
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
-        }
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 50000
-};
+// const shoppingMallData = {
+//     shops: [{
+//             width: 10,
+//             length: 5
+//         },
+//         {
+//             width: 15,
+//             length: 7
+//         },
+//         {
+//             width: 20,
+//             length: 5
+//         },
+//         {
+//             width: 8,
+//             length: 10
+//         }
+//     ],
+//     height: 5,
+//     moneyPer1m3: 30,
+//     budget: 50000
+// };
 
-function isBudgetEnough(data) {
-    let vShops = 0;
-    // получили значения из всех {} в []shops;
-    for (let i = 0; i < data.shops.length; i++) {
-        if (typeof (data.shops[i]) === 'object') {
+// function isBudgetEnough(data) {
+//     let vShops = 0;
+//     // получили значения из всех {} в []shops;
+//     for (let i = 0; i < data.shops.length; i++) {
+//         if (typeof (data.shops[i]) === 'object') {
 
-            let {
-                width,
-                length
-            } = data.shops[i];
+//             let {
+//                 width,
+//                 length
+//             } = data.shops[i];
 
-            vShops += width * length;
-            //console.log(vShops);
-        }
-    }
+//             vShops += width * length;
+//             //console.log(vShops);
+//         }
+//     }
 
-    const totalCost = vShops * data.height * data.moneyPer1m3;
-    //console.log(totalCost);
+//     const totalCost = vShops * data.height * data.moneyPer1m3;
+//     //console.log(totalCost);
 
-    if (totalCost <= data.budget) {
-        return `Бюджета достаточно`;
-    } else {
-        return `Бюджета недостаточно`;
-    }
+//     if (totalCost <= data.budget) {
+//         return `Бюджета достаточно`;
+//     } else {
+//         return `Бюджета недостаточно`;
+//     }
 
+// }
+
+// isBudgetEnough(shoppingMallData);
+// // Task # 37. 13 (shops budget) solved
+
+// // Task # 37. 14 (list of students)
+
+// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+// function sortStudentsByGroups(arr) {
+//     arr.sort();
+//     console.log(arr);
+//     const newArr = [];
+
+//     for (let i = 0; i < 9; i += 3) {
+//         newArr.push(arr.slice(i, i + 3));
+//     }
+
+//     if (arr.length == 9) {
+//         newArr.push('Оставшиеся студенты: -');
+//         return newArr;
+
+//     } else {
+//         newArr.push(`Оставшиеся студенты: ${arr[9]}`);
+//         for (let j = 10; j < arr.length; j++) {
+//             newArr[3] += `, ${arr[j]}`;
+//         }
+//         return newArr;
+//     }
+// }
+// sortStudentsByGroups(students);
+
+// // Task # 37. 14 (list of students). Solved with google help.
+
+
+// let text1 = "some text is here";
+// text1 = text1.split(' '); // ['some', 'text', 'is', 'here'];
+// console.log(text1);
+
+
+// let text2 = ['some', 'text', 'is', 'here'];
+// text2 = text2.join('> ');
+// console.log(text2);
+
+// let a = null;
+// a = String(a);
+// console.log(typeof (a)); // 5 как строка
+
+// console.log(typeof (Number('77'))); // 77 тип number
+
+
+
+// console.log(+'88f8'); // 77 тип number
+
+// console.log(parseInt('15px', 10)); //15
+// console.log(parseInt('100px', 25)); //15
+
+// let param = 01;
+
+// if (param) {
+//     console.log('working >>> true');
+// } // ничего не выведет, так как param == 0 то есть false
+
+// console.log(typeof (Boolean('77'))); // тип boolean
+// console.log(Boolean(''));
+
+// console.log(!!('')); // false
+// console.log(!!(5)); // true
+
+
+function createCounter() {
+    let counter = 0;
+    const myFunction = function () { // обьявление новой функции myFunction через cosnt 
+        //let counter = 0; в таком случае вернет х1 х2 х3 = 1
+        counter = counter + 1;
+        return counter;
+    };
+    return myFunction;
 }
+const incr = createCounter();
 
-isBudgetEnough(shoppingMallData);
-// Task # 37. 13 (shops budget) solved
+let x1 = incr();
+x1 = incr();
+const x2 = incr();
+const x3 = incr();
 
-// Task # 37. 14 (list of students)
+console.log(incr); // функция myFunction
+console.log(x1, x2, x3); // 1 2 3
 
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
 
-function sortStudentsByGroups(arr) {
-    arr.sort();
-    console.log(arr);
-    const newArr = [];
-
-    for (let i = 0; i < 9; i += 3) {
-        newArr.push(arr.slice(i, i + 3));
-    }
-
-    if (arr.length == 9) {
-        newArr.push('Оставшиеся студенты: -');
-        return newArr;
-
-    } else {
-        newArr.push(`Оставшиеся студенты: ${arr[9]}`);
-        for (let j = 10; j < arr.length; j++) {
-            newArr[3] += `, ${arr[j]}`;
-        }
-        return newArr;
-    }
-}
-sortStudentsByGroups(students);
-
-// Task # 37. 14 (list of students). Solved with google help.
+let c = 4;
+const addX = x => n => n + x;
+const addThree = addX(3);
+let d = addThree(c);
+console.log('example partial application', d);
--- VS1/script2.js
+++ VS1/script2.js
@@ -1,67 +1,14 @@
-"use strict";
+'use strict';
 
-let numberOfFilms,
-    filmName,
-    score;
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-// 1st question
-first: for (let i = 0; i < 1; i++) {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели? Введите число от 1', '');
-    if (numberOfFilms > 0) {
-        personalMovieDB.count = numberOfFilms;
-        break first;
-    } else {
-        i--;
-    }
-}
-console.log(`Вопрос №1. Фильмов: ${numberOfFilms}`);
-
-// 2nd question
-second: for (let i = 0; i < 1; i++) {
-    filmName = prompt('Вопрос 2. Один из последних посмотренных фильмов?\nОт 4х символов', '');
-
-    if (filmName != null && filmName.length > 3 && filmName.length < 50) {
-        //break second;
-    } else {
-        i--;
-    }
-}
-console.log(`Вопрос №2. Наименование фильма: ${filmName}`);
-
-// 3rd question
-third: for (let i = 0; i < 1; i++) {
-    score = prompt('На сколько оцените его? (от 1 до 10)', '');
-
-    if (score > 0 && score <= 10) {
-        personalMovieDB.movies[filmName] = score;
-        break third;
-    } else {
-        i--;
-    }
-}
-// solved steps 1-3 - commited
-
-console.log(`Вопрос №3. Рейтинг: ${score}`);
-
-for (let i = 0; i < 1; i++) {
-    if (personalMovieDB.count < 10) {
-        console.log(`Просмотрено довольно мало фильмов: ${personalMovieDB.count}`);
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log(`Вы классический зритель: ${personalMovieDB.count} фильмов`);
-    } else if (personalMovieDB.count >= 30) {
-        console.log(`Вы киноман: ${personalMovieDB.count} фильмов`);
-    } else {
-        console.log('error vs');
-    }
-}
-
-console.log(personalMovieDB);
-// finished task # 24.2 updated
+// Какое будет выведено значение: let x = 5; alert( x++ ); ?// == 5 так как ПОСТФИКСНАЯ запись
+// Чему равно такое выражение: [ ] + false - null + true ?//   error? undefined? == string 'NaN'
+// Что выведет этот код: let y = 1; let x = y = 2; alert(x); ?// 2
+// Чему равна сумма [ ] + 1 + 2? // '12' string
+// Что выведет этот код: alert( "1"[0] )? // == 1
+// Чему равно 2 && 1 && null && 0 && undefined ? // false - null
+// Есть ли разница между выражениями? !!( a && b ) и (a && b)? // == false (true и b)
+// Что выведет этот код: alert( null || 2 && 3 || 4 ); ? // true ==  3 
+// a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?// false ?
+// Что выведет этот код: alert( +"Infinity" ); ?// == Infinity как число
+// Верно ли сравнение: "Ёжик" > "яблоко"?// false
+// Чему равно 0 || "" || 2 || undefined || true || falsе ? // (2) true
