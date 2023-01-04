ff08f401cfefdd96566071af9390e3744ad677aa
Vadim <sabalayev1@gmail.com>
2023-01-04 16:38:56 +0200

    solved 1-3 tasks from movies + elements

--- VS1/projectUD/js/script.js
+++ VS1/projectUD/js/script.js
@@ -24,3 +24,23 @@ const movieDB = {
     ]
 };
 
+const ads = document.getElementsByClassName('promo__adv'),
+    genre = document.getElementsByClassName('promo__genre'),
+    promoBG = document.getElementsByClassName('promo__bg');
+
+// 1) revome ADs
+ads[0].remove();
+
+// 2) change genre
+const div = document.createElement('div');
+div.classList.add('promo__genre');
+//genre[0].after(div1); - добавляет елемент в хтмл (не обязательно если  делать replace)
+div.innerHTML = 'ДРАМА'; // добавляет текст в div тег
+genre[0].replaceWith(div); // сам реплейс
+
+// 3) change image bg
+
+//promoBG[0].style.background = 'url("../img/mars.webp") center center/cover no-repeat;';
+promoBG[0].style.cssText = 'background: url("./img/bg.jpg") center center/cover no-repeat;';
+
+// 4) sorting films
